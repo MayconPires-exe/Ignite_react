587bef094ef5166089415dbe7c2d711aa9574ed3
Maka <piresmaycon4@gmail.com>
2022-03-14 14:13:24 -0300

    test

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,4 +1,7 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
 
 const repository = {
   name: 'unform',
@@ -8,8 +11,16 @@ const repository = {
 
 // creating a new component
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
+
   return (
-    <section>
+    <section className="repository-list">
       <h1>Repository List</h1>
 
       <ul>
