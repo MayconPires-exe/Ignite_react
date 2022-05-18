5f7ca7cdc215627a48e45ae9ec00bb11fa80dc90
Maycon Pires <piresmaycon4@gmail.com>
2022-05-18 10:17:00 -0300

    Style complete

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -31,6 +31,15 @@ export const GlobalStyle = createGlobalStyle`
   }
   // REM => 1rem = font-size da pag.
 
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
