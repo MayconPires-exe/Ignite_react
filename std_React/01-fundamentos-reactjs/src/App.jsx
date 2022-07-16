573fbab042a175b412a4b14c03794540a041d458
Maycon Pires <piresmaycon4@gmail.com>
2022-07-16 10:42:31 -0300

    Push all

--- std_React/01-fundamentos-reactjs/src/App.jsx
+++ std_React/01-fundamentos-reactjs/src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
+
+import styles from './App.module.css'
 
 import './global.css';
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
 
-      <Post
-        author="Ingrid Barros"
-        content="Muito ipsum"
-      />
-      <Post
-        author="Maycon"
-        content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit at, non fugiat nam exercitationem cum culpa iure aliquid modi iste facilis dolores soluta in quos perferendis assumenda vero eligendi maxime."
-      />
-    </>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+           <Post
+            author="Ingrid Barros"
+            content="Muito ipsum"
+          />
+          <Post
+            author="Maycon"
+            content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit at, non fugiat nam exercitationem cum culpa iure aliquid modi iste facilis dolores soluta in quos perferendis assumenda vero eligendi maxime."
+          />
+        </main>
+      </div>
+    </div>
   )
 }
